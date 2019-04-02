e6b8b699763935bb33f4cf9ecb595d29f9006d03
peterianmuller <peterianmuller@gmail.com>
2019-04-01 22:45:21 -0700

    Add basic ReadMe and repos with projects

---
+++ README.md
@@ -0,0 +1,10 @@
+This repo houses two projects for the end of SSP. We'll be diving into how a JavaScript file works with HTML and CSS file to create an interactive web application.
+
+Learning Goals:
+
+How functions can be used to update the DOM elements (HTML elements) displayed on the browser
+How JavaScript can listen for user interaction events, like clicks, and update the page
+How Javascritp variables can be used to update the page
+
+Clicker game derived from https://codepen.io/684386/pen/VaYqKY
+Stopwatch derived from https://codepen.io/_Billy_Brown/pen/dbJeh
---
+++ clicker/main.js
@@ -0,0 +1,40 @@
+//variables
+var clicks = 0; //increment this by one every click
+var click_increment = 1; //how many clicks per click
+//functions
+
+function updateTotalClicks() {
+    //updates the number of clicks
+    var e = document.getElementById('total-clicks');
+    e.innerHTML = clicks;
+}
+
+//click events
+document.querySelector('.add-click').onclick = function() {
+    clicks += click_increment;
+    updateTotalClicks(); //updates the text
+    if (clicks > 5) {
+        TODO;
+        // if clicks === 50 add animation
+    }
+};
+
+document.querySelector('.remove-click').onclick = function() {
+    clicks -= parseFloat(click_increment);
+    updateTotalClicks(); //updates the text
+};
+
+//Increase Click Increment
+// Let's increase the amount of clicks!
+document.getElementById('increase_clicks').onclick = function() {
+    if (clicks >= 5) {
+        var upgrade_cost = 5;
+        clicks -= 5;
+        updateTotalClicks();
+        click_increment++;
+        var clicksBeingAddedSpan = document.querySelector('.clicks-being-added');
+        clicksBeingAddedSpan.innerHTML = click_increment;
+    } else {
+        alert('You need more clicks! You only have ' + clicks + ' clicks!');
+    }
+};
